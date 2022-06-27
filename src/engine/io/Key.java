dc873f27f0a6b06be4a79e755f444d1f6c817dbf
Mario Susanto <msus0004@student.monash.edu>
2022-06-27 15:30:49 +0800

    minor change to Key and removed TTest

--- src/engine/io/Key.java
+++ src/engine/io/Key.java
@@ -9,4 +9,10 @@ public class Key {
     public static boolean pressed(String key) {
         return App.getInstance().pressed(key);
     }
+    public static boolean bothHeld(String key1, String key2) {
+        return Key.held(key1) && Key.held(key2);
+    }
+    public static boolean bothPressed(String key1, String key2) {
+        return Key.pressed(key1) && Key.pressed(key2);
+    }
 }
